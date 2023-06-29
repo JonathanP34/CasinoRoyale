e8dd8445057cb65b3aa86c0ad922751dd44f6e29
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2023-06-29 19:42:32 -0400

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class MainMenu extends State {
-    public MainMenu() {
-        super();
+    public MainMenu(Game g, Player p) {
+        super(g, p);
     }
 
     public void render() { //Displaying the options in the menu
@@ -15,30 +15,17 @@ public class MainMenu extends State {
 
     public void update() {
         Scanner userInput = new Scanner(System.in);
-        int choice;
+        int choice = 0;
         boolean done = false;
         //For loop to make sure you get the proper
+
         while (done == false) {
             try {
                 String val = userInput.nextLine();
                 choice = Integer.parseInt(val);
 
                 if (choice > 0 && choice < 5) {
-                    switch(choice) {
-                        case 1:
-                            ChooseGame cg = new ChooseGame();
-                            cg.enterState();
-                            break;
-                        case 2:
-                            getPlayer().addBalance();
-                            break;
-                        case 3:
-                            //Stat State <- Saving this for later because I want to choose the stats from each game in specific, poker =/= slots
-                            break;
-                        case 4:
-                            exitState();
-                            break;
-                    }
+                    done = true;
                 } else {
                     System.out.println("Error 3");
                     System.out.println("Please enter a valid value corresponding to the menu options above.");
@@ -48,6 +35,29 @@ public class MainMenu extends State {
                 System.out.println("Please enter valid integer value.");
             }
         }
+
+        switch(choice) {
+            case 1:
+                ChooseGame cg = new ChooseGame(getGame(), getPlayer()); // <- Problem here
+                cg.enterState(); //Entering the Choose Game State
+                //System.out.println(getGame().getStateStack().peek().toString());
+                break;
+            case 2:
+                System.out.println("\n"); //Adding space before the balance
+                getPlayer().addBalance();
+                break;
+            case 3:
+                //Stat State <- Saving this for later because I want to choose the stats from each game in specific, poker =/= slots
+                break;
+            case 4:
+                exitState(); //Exiting the menu state
+                break;
+        }
+
+    }
+
+    public String toString() {
+        return "Main Menu State";
     }
 
 }
