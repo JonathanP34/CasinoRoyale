12207f759995eecfc4083c8b3a01a1c6437f4a12
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2023-06-29 19:42:54 -0400

    Update State.java

--- State.java
+++ State.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class State {
     private Game game;
-    private State prevState;
     public Player player;
 
     //Constructor
-    public State() {
-        game = new Game();
-        prevState = null;
+    public State(Game g, Player p) {
+        game = g;
+        player = p;
+
     }
 
     public void update() {
@@ -15,42 +15,34 @@ public class State {
     public void render() {
     }
     public void enterState() {
-        if (game.getStateStack().size() > 1) {
-            prevState = game.getStateStack().peek();
-        }
         game.getStateStack().push(this); //Pushing its own state on the stack
     }
 
     public void exitState() {
         game.getStateStack().pop();
-        if (game.getStateStack().size() > 1) { //If there is more than one state
-            State temp = game.getStateStack().pop(); //Pop off, get the previous state then push back on
-            prevState = game.getStateStack().peek();
-            game.getStateStack().push(temp);
-        } else { //If there is only 1 state left
-            prevState = null;
+        if (game.getStateStack().size() == 1) {
             System.exit(0);
         }
     }
 
     public void scroll() {
         try {
-            for (int i = 0; i < 10; i++) {
-                System.out.println("\n");
-                Thread.sleep(30);
+            for (int i = 0; i < 15; i++) {
+                System.out.println("");
+                Thread.sleep(55);
             }
         } catch (Exception e) {
             System.out.println("Error Code 1");
         }
     }
 
-    //Setters and Getters
-    public State getPrevState() {
-        return prevState;
-    }
-    public void setPrevState(State newPrevState) {
-        prevState = newPrevState;
+    public String toString() {
+        return "State";
     }
 
+    //Setters and Getters
+    public Game getGame() {
+        return game;
+    }
     public Player getPlayer() {return player;}
 }
