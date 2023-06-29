134452ba82c0ee0c4f81be7d43e094430d868254
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2023-06-29 19:43:05 -0400

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,23 +1,25 @@
 import java.util.*;
 public class Title extends State{
-    public Title() {
-        super();
+    public Title(Game g, Player p) {
+        super(g, p);
     }
     public void update() {
         //Getting the user input, if they type anything the game begins
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Type anything to continue: ");
         String text = userInput.nextLine();
         if (text.length() > 0) {
-            MainMenu menuState = new MainMenu();
+            MainMenu menuState = new MainMenu(getGame(), getPlayer());
             menuState.enterState(); //Entering the menuState
         }
     }
 
     public void render() {
-        scroll();
         System.out.println("*****************************");
         System.out.println("*       CASINO ROYALE       *");
         System.out.println("*****************************\n");
+    }
+
+    public String toString() {
+        return "Title State";
     }
 }
